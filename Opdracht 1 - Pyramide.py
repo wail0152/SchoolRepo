c9922d03b713620fec7c8e833921df1436a915ed
programmer0152 <abou.w@hotmail.com>
2020-02-04 14:36:05 +0100

    Eerste versie

--- Opdracht 1 - Pyramide.py
+++ Opdracht 1 - Pyramide.py
@@ -1,1 +1,9 @@
-print("test")
+n = int(input("Hoe groot wil je de piramide maken? ")) * 2
+t = 1
+
+for i in range(n - 1):
+    amount = min(t, n - t)
+    for j in range(amount):
+        print("*", end="")
+    print("", end="\n")
+    t += 1

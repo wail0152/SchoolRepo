853dc27adcbdee900710deb1b593c2853bca5ed3
programmer0152 <abou.w@hotmail.com>
2020-02-04 14:44:20 +0100

    Eerste versie

--- Opdracht 1 - Pyramide.py
+++ Opdracht 1 - Pyramide.py
@@ -1,9 +1,28 @@
 n = int(input("Hoe groot wil je de piramide maken? ")) * 2
 t = 1
 
-for i in range(n - 1):
-    amount = min(t, n - t)
-    for j in range(amount):
-        print("*", end="")
-    print("", end="\n")
-    t += 1
+
+def forLoop(t, n):
+    for i in range(n - 1):
+        amount = min(t, n - t)
+        for j in range(amount):
+            print("*", end="")
+        print("", end="\n")
+        t += 1
+
+
+def whileLoop(t, n):
+    i = 0
+    j = 0
+    while i < n - 1:
+        amount = min(t, n - t)
+        while j < amount:
+            print("*", end="")
+            j += 1
+        print("", end="\n")
+        t += 1
+        i += 1
+        j = 0
+
+
+whileLoop(t, n)

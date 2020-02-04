86f00fafed33d9a0d8176ce2195fbd40c0ea18c5
programmer0152 <abou.w@hotmail.com>
2020-02-04 20:57:29 +0100

    4de klaar

--- Opdracht 4 - Palindroom.py
+++ Opdracht 4 - Palindroom.py
@@ -1,3 +1,10 @@
-def checkPalindroom():
-    return None
+def checkPalindroomSelf(s):
+    newString = ""
+    for i in s:
+        newString = i + newString
+    return newString
+
+
+def checkPalindroomLib(s):
+    return "".join(list(reversed(s)))
 

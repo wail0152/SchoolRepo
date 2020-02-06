b7dbed7057f07dfd3fafa97d454f281dbfc45424
programmer0152 <abou.w@hotmail.com>
2020-02-06 15:21:07 +0100

    Opdracht 10 verbeterd.

--- Formatieve opdracht 1 algoritmisch programmeren/Opdracht 10 - Fibonaci.py
+++ Formatieve opdracht 1 algoritmisch programmeren/Opdracht 10 - Fibonaci.py
@@ -1,8 +1,2 @@
-fn = 0
-
-
 def fibonacci(n):
-    return 1 if n == 1 or n == 2 else fibonacci(n - 1) + fibonacci(n - 2)
-
-
-print(fibonacci(9))
+    return 1 if n < 2 else fibonacci(n - 1) + fibonacci(n - 2)

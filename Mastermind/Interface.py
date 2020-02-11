8087137bf4d46a2596351159c75a26cda721faff
programmer0152 <abou.w@hotmail.com>
2020-02-11 23:39:44 +0100

    Prototype (1 game mode, niet werkende witte pin en geen winconditie)

--- Mastermind/Interface.py
+++ Mastermind/Interface.py
@@ -2,17 +2,63 @@ from Mastermind.definitions import *
 
 game_modes = {0: "MAN VS COMPUTER", 1: "COMPUTER VS MAN"}
 option = -1
+current_column = 0
 
 while option != 0 and option != 1:
     try:
         option = int(input(f"Type 0 for {game_modes[0]} and 1 for {game_modes[1]}: "))
     except ValueError:
         print("Try again and input 0 or 1.\n")
-print(f"You have chosen {game_modes[option]}.\n")
+print(f"You have chosen {game_modes[option]}.")
+
+
+def man_vs_ai(col):
+    check_end()
+    game_board[col] = get_moves()
+    print_board()
+    give_feedback()
+    return col + 1
+
+
+def check_end():
+    if current_column == 10:
+        print(f"The right combination is {shield_row}.")
+        exit()
+
+
+def get_moves():
+    moves = str(input(f"\n({10 - current_column} move(s) left) Input 4 colors with a comma:\n")).split(",")
+    if len(moves) < 4:
+        print("Not enough colors given.")
+        return get_moves()
+    else:
+        for move in moves:
+            if move not in colors:
+                print("Not a valid color has been given.")
+                return get_moves()
+    return moves
+
+
+def print_board():
+    for column in range(len(game_board)):
+        for row in range(len(game_board[column])):
+            print(game_board[column][row], end="  ")
+        print("")
+
+
+def give_feedback():
+    red_pins = 0
+    white_pins = 0
+    for row in range(len(game_board[current_column])):
+        if game_board[current_column][row] == shield_row[row]:
+            red_pins += 1
+        elif game_board[current_column][row] in shield_row:
+            white_pins += 1
+    print(f"You have {red_pins} red pins and {white_pins} white pins.")
 
 
 shield_row = set_shield(rand_shield())
-print(shield_row)
 running = True
 while running:
-    input("Wait")
+    if option == 0:
+        current_column = man_vs_ai(current_column)
--- Mastermind/definitions.py
+++ Mastermind/definitions.py
@@ -4,7 +4,7 @@ colors = ['r', 'g', 'b', 'o', 'y', 'p']
 shield_row = []
 game_board_width = 10
 game_board_height = 4
-game_board = [[0 for y in range(game_board_height)] for x in range(game_board_width)]
+game_board = [[0 for x in range(game_board_height)] for y in range(game_board_width)]
 
 
 def set_shield(shield):

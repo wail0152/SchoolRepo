90a23c7d7bd15c669466240f9d383e7aef59d02f
programmer0152 <abou.w@hotmail.com>
2020-02-13 16:07:28 +0100

    Beter opgedeeld.

--- Mastermind/definitions.py
+++ Mastermind/definitions.py
@@ -5,6 +5,7 @@ shield_row = []
 game_board_width = 10
 game_board_height = 4
 game_board = [[0 for x in range(game_board_height)] for y in range(game_board_width)]
+current_column = 0
 
 
 def get_shield(shield):
@@ -13,3 +14,28 @@ def get_shield(shield):
 
 def rand_shield():
     return random.sample(colors, 4)
+
+
+def check_end():
+    if current_column == 10:
+        print(f"The right combination is {shield_row}.")
+        exit()
+
+
+def check_win(rp):
+    if rp == 4:
+        print("You have won!")
+        exit()
+
+
+def get_moves():
+    moves = str(input(f"\n({10 - current_column} move(s) left) Input 4 colors with a comma:\n")).split(",")
+    if len(moves) < 4:
+        print("Not enough colors given.")
+        return get_moves()
+    else:
+        for move in moves:
+            if move not in colors:
+                print("Not a valid color has been given.")
+                return get_moves()
+    return moves
--- Mastermind/interface.py
+++ Mastermind/interface.py
@@ -1,4 +1,4 @@
-from Mastermind.functions import *
+from Mastermind.definitions import *
 
 game_modes = {0: "MAN VS COMPUTER", 1: "COMPUTER VS MAN"}
 option = -1
@@ -11,8 +11,54 @@ while option != 0 and option != 1:
 print(f"You have chosen {game_modes[option]}.")
 
 
+def man_vs_ai(col):
+    check_end()
+    game_board[col] = get_moves()
+    print_board()
+    give_feedback()
+    return col + 1
+
+
+def ai_vs_man(col):
+    check_end()
+    game_board[col] = get_moves()
+    print_board()
+    give_feedback()
+    return col + 1
+
+
+def print_board():
+    for column in range(len(game_board)):
+        for row in range(len(game_board[column])):
+            print(game_board[column][row], end="  ")
+        print("")
+
+
+def give_feedback():
+    red_pins = 0
+    white_pins = 0
+    visited = []
+
+    for guess in range(len(game_board[current_column])):
+        if game_board[current_column][guess] == shield_row[guess]:
+            red_pins += 1
+            visited.append(guess)
+
+    for guess in range(len(game_board[current_column])):
+        for color in range(len(shield_row)):
+            if game_board[current_column][guess] == shield_row[color] and guess != color and color not in visited:
+                white_pins += 1
+                visited.append(color)
+
+    print(f"You have {red_pins} red pins and {white_pins} white pins.")
+    check_win(red_pins)
+    return (red_pins, white_pins)
+
+
 shield_row = get_shield(rand_shield())
 running = True
 while running:
     if option == 0:
         current_column = man_vs_ai(current_column)
+    else:
+        current_column = ai_vs_man(current_column)
